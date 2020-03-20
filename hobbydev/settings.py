3ceb3347a6af76cd2abe79c2b4319463b6f162e5
katana <roduor41@gmail.com>
2020-03-20 10:00:56 +0300

    Add allowed hosts and python version to 3.6

--- hobbydev/settings.py
+++ hobbydev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ky!#$+spm#su&ginfqtz2$l1lpnt#9)v5z6xs$vcrscv9m0-wc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
